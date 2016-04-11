a4c692ba93542176af94842071da50dd73c5a62e
Monte Thakkar <mthakkar@mail.sfsu.edu>
2016-04-11 16:02:15 -0700

    Xcode proj created with new emoji's and basic pan and touch of tray

--- Emoji-Artbook/Emoji-Artbook/ViewController.swift
+++ Emoji-Artbook/Emoji-Artbook/ViewController.swift
@@ -10,11 +10,54 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var trayView: UIView!
+    
+    var trayOriginalCenter: CGPoint!
+    var trayCenterWhenOpen: CGPoint!
+    var trayCenterWhenClosed: CGPoint!
+    
+    var newlyCreatedFace: UIImageView!
+    var faceOriginalCenter: CGPoint!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        trayCenterWhenOpen = trayView.center
+        trayCenterWhenClosed = CGPoint(x: 160, y: 660)
     }
-
+    
+    @IBAction func onTrayPanGesture(panGestureRecognizer: UIPanGestureRecognizer) {
+        let velocityY = panGestureRecognizer.velocityInView(trayView).y
+        let xPoints = CGFloat(floatLiteral: 320.0)
+        let duration: NSTimeInterval = (Double) (xPoints/velocityY)
+        
+        if panGestureRecognizer.state == UIGestureRecognizerState.Began {
+            trayOriginalCenter = trayView.center
+        } else if panGestureRecognizer.state == UIGestureRecognizerState.Changed && velocityY > 0 {
+            UIView.animateWithDuration(duration, delay: 0, usingSpringWithDamping: 0.3, initialSpringVelocity: 7.0, options: UIViewAnimationOptions.CurveLinear , animations: {
+                self.trayView.center = self.trayCenterWhenClosed
+                }, completion: nil)
+        } else if panGestureRecognizer.state == UIGestureRecognizerState.Changed && velocityY < 0 {
+            UIView.animateWithDuration(duration, animations: {
+                self.trayView.center = self.trayCenterWhenOpen
+            })
+        }
+    }
+    
+    @IBAction func onTrayTapGesture(tapGestureRecognizer: UITapGestureRecognizer) {
+        if trayView.center == trayCenterWhenOpen {
+            UIView.animateWithDuration(2.0, delay: 0, usingSpringWithDamping: 0.3, initialSpringVelocity: 7.0, options: UIViewAnimationOptions.CurveLinear , animations: {
+                self.trayView.center = self.trayCenterWhenClosed
+                }, completion: nil)
+            print("Closing tray")
+        } else {
+            UIView.animateWithDuration(0.4, animations: {
+                self.trayView.center = self.trayCenterWhenOpen
+            })
+            print("Opening tray")
+        }
+    }
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
